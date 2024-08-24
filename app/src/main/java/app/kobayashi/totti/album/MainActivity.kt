45db2e2171eb2fe29c25b0471ab9771e52b0071d
taka-cell <k.taka160724@gmail.com>
2024-08-25 00:09:47 +0900

    最初のコミットメッセージ

---
+++ app/src/main/java/app/kobayashi/totti/album/MainActivity.kt
@@ -0,0 +1,56 @@
+package app.kobayashi.totti.album
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import app.kobayashi.totti.album.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        val preview = Intent(this, PreviewActivity::class.java)
+
+        binding.albumImage1.setOnClickListener{
+            preview.putExtra("image",R.drawable.sea_image)
+            preview.putExtra("title","海")
+            startActivity(preview)
+        }
+        binding.albumImage2.setOnClickListener{
+            preview.putExtra("image",R.drawable.cat_image)
+            preview.putExtra("title","猫")
+            startActivity(preview)
+        }
+
+        binding.albumImage3.setOnClickListener{
+            preview.putExtra("image",R.drawable.dog_image)
+            preview.putExtra("title","子犬")
+            startActivity(preview)
+        }
+
+        binding.albumImage4.setOnClickListener{
+            preview.putExtra("image",R.drawable.cake_image)
+            preview.putExtra("title","ケーキ")
+            startActivity(preview)
+        }
+
+        binding.albumImage5.setOnClickListener{
+            preview.putExtra("image",R.drawable.night_image)
+            preview.putExtra("title","夜景")
+            startActivity(preview)
+        }
+
+        binding.albumImage6.setOnClickListener{
+            preview.putExtra("image",R.drawable.flower_image)
+            preview.putExtra("title","桜")
+            startActivity(preview)
+        }
+
+
+    }
+}
